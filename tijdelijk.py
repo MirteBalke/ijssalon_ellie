0610de93a1420263e42af0cde2e47d9ba7db0448
Mirte <m.balke@lumc.nl>
2024-07-16 14:26:30 +0200

    bestand tijdelijk.py gecodeerd

--- tijdelijk.py
+++ tijdelijk.py
@@ -3,10 +3,18 @@ prijzen = {
     "vanille" : 4,
     "chocolade" : 5
     }
-aanbieding = ((prijzen[0]*0.8))
+aanbieding = ((prijzen["aardbei"]*0.8))
 
-print(aanbieding)
+reclame_tekst = f"Vandaag in de aanbieding: vanille-ijs, 1 liter - slechts €{aanbieding}"
 
-##reclame_tekst = "Vandaag in de aanbieding: vanille-ijs, 1 liter - slechts €{aanbieding}
+reclame_tekst2 = reclame_tekst[:62]
 
-##mijn_dictionary["reclame_tekst"] = (f"Vandaag in de aanbieding: vanille-ijs, 1 liter - slechts €{aanbieding}")
+reclame_tekst3 = reclame_tekst2.upper()
+
+reclame_tekst4 = reclame_tekst3.split(" ")
+
+for el in reclame_tekst4:
+    if len(el) <= 4:
+        print(el.lower())
+    else:
+        print(el)
