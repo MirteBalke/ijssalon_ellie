23e3f93269e452eee206a56e033c6a7bb5740ba6
Mirte <m.balke@lumc.nl>
2024-07-16 11:15:11 +0200

    bestand helper.py nogmaals toegevoegd)

---
+++ tijdelijk.py
@@ -0,0 +1,12 @@
+prijzen = {
+    "aardbei" : 3,
+    "vanille" : 4,
+    "chocolade" : 5
+    }
+aanbieding = ((prijzen[0]*0.8))
+
+print(aanbieding)
+
+##reclame_tekst = "Vandaag in de aanbieding: vanille-ijs, 1 liter - slechts €{aanbieding}
+
+##mijn_dictionary["reclame_tekst"] = (f"Vandaag in de aanbieding: vanille-ijs, 1 liter - slechts €{aanbieding}")
